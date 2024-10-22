928f1b515dc3569916aea13a8f069befc533f2a6
Arthur <crfernandesarthur@gmail.com>
2024-10-22 17:59:04 -0300

    confirmação de pagamento ok

--- app.py
+++ app.py
@@ -40,12 +40,32 @@ def get_image(file_name):
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
+  data = request.get_json()
+  
+  if "bank_payment_id" not in data and "value" not in data:
+    return jsonify({'message': 'Invalid payment data.'}), 400
+  
+  payment = Payment.query.filter_by(bank_payment_id=data.get('bank_payment_id')).first()
+  
+  if not payment or payment.paid:
+    return jsonify({'message': 'Payment not found.'}), 404
+  
+  if data.get('value') != payment.value:
+    return jsonify({'message': 'Invalid payment data.'}), 400
+  
+  payment.paid = True
+  db.session.commit()
+  socketio.emit(f'payment-confirmed-{payment.id}')
   return jsonify({'message': 'The payment has been confirmed.'})
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
   payment = Payment.query.get(payment_id)
   
+  if payment.paid:
+    return render_template('confirmed_payment.html', 
+                         payment_id= payment.id, 
+                         value=payment.value)
   
   return render_template('payment.html', 
                          payment_id= payment.id, 
